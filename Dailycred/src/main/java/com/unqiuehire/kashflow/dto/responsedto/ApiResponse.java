606b8ab46090aedc7e2567cd2ddb3f188efc6419
unknown <Varungoud06>
2026-04-09 11:30:27 +0530

    made project structure

---
+++ Dailycred/src/main/java/com/unqiuehire/kashflow/dto/responsedto/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.unqiuehire.kashflow.dto.responsedto;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+    private Object error;
+}
